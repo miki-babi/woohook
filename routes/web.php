c2cd4b4b7488539d9c759dfb21f17ef09fa72c2b
miki-babi <mikiyasshiferaw@gmail.com>
2025-07-01 20:30:48 +0300

    testing

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,18 @@
 <?php
-
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
 Route::get('/', function () {
     return view ('welcome');
 });
-Route::post('/test', function () {
+Route::post('/test', function (Request $request) {
+    // Handle the request here
+    // For example, you can access the request data like this:
+    // $data = $request->all();
+    Log::info('Test route accessed', [
+        'request_data' => $request->all(),
+    ]);
     return  ('welcome');
 })->withoutMiddleware([VerifyCsrfToken::class]);
